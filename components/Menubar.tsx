1170aa52d2a2a73f815e773909516192ba04d3d5
sasirat <sasirat019@gmail.com>
2021-09-02 23:55:45 +0700

    Add side bar

---
+++ components/Menubar.tsx
@@ -0,0 +1,18 @@
+import stylesMenubar from '../styles/MenuBar.module.css'
+
+const MenuBar = () => {
+  return (
+    <div className={`${stylesMenubar.bg}`}>
+      <div className="text-center py-8 text-white text-xl">
+        <h1>NVSL - IMS</h1>
+      </div>
+      <div className="mt-20 text-center text-white font-bold">
+        <div className="my-8">Dashboard</div>
+        <div className="my-8">Partner</div>
+        <div className={`${stylesMenubar.select} my-8 text-gray-500 p-2 ml-4 rounded-l-full relative`}>Invoice</div>
+      </div>
+    </div>
+  )
+}
+
+export default MenuBar
